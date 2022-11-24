0dde94310dea1450822ee979c9b01eb7b20e7a87
TrevorHenry05 <trevorjhen03@gmail.com>
2022-11-23 23:26:38 -0800

    feat: create dungeon

--- DungeonAdventureProject/src/Model/Dungeon.java
+++ DungeonAdventureProject/src/Model/Dungeon.java
@@ -3,6 +3,7 @@ package Model;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
+import Utility.Utility;
 
 public class Dungeon implements Serializable {
 	
@@ -22,8 +23,8 @@ public class Dungeon implements Serializable {
 	}
 	
 
-	public DungeonRoom[][] createDungeon() {
-		DungeonRoom[][] d = new DungeonRoom[4][4];
+	private static DungeonRoom[][] createDungeon() {
+		DungeonRoom[][] d = new DungeonRoom[7][7];
 		MonsterFactory mf =  new MonsterFactory();
 		ItemFactory If = new ItemFactory();
 		
@@ -31,100 +32,150 @@ public class Dungeon implements Serializable {
 		Monster monster;
 		List<Item> items;
 		
-		monster = null;
-		items = new ArrayList<Item>();
-		room = new DungeonRoom(items, monster, false, true, false, true, false, true, false);
-		d[0][0] = room;
+		for(int i = 0; i < d.length; i++) {
+			for(int j = 0; j < d[i].length; j++) {
+				boolean north = false, west = false, east = false, south = false;				
+				monster = null;
+				items = new ArrayList<Item>();
+				room = null;
+				
+				if(!((i == 0 && j == 0) || ((i == (d.length - 1)) && (j == (d[i].length - 1))))) {
+					//add heals
+					int chance = Utility.randomNumberGen(0, 100);
+					if(chance < 10) {
+						items.add(If.createItem("heal"));
+					} else if (chance >= 10 && chance < 20) {
+						items.add(If.createItem("heal"));
+						items.add(If.createItem("heal"));
+					}
+					
+					//add vision
+					chance = Utility.randomNumberGen(0, 100);			
+					if(chance < 10)  {
+						items.add(If.createItem("vision"));
+					}
+					
+					//add monster
+					chance = Utility.randomNumberGen(0, 100);
+					if(chance < 5) {
+						chance = Utility.randomNumberGen(0, 100);
+						if(chance < 33) {
+							monster = mf.createMonster("ogre");
+						} else if(chance >= 33 && chance < 66) {
+							monster = mf.createMonster("skeleton");
+						} else {
+							monster = mf.createMonster("gremlin");
+						}
+					}
+					
+					north = isNorthDoor(d, i, j);
+					west = isWestDoor(d, i, j);
+					south = isSouthDoor(d, i, j);
+					east = isEastDoor(d, i, j);
+					
+					room = new DungeonRoom(items, monster, north, south, west, east, false, false, false);
+				} else {
+					if(i == 0 && j == 0) {
+						south = isSouthDoor(d, i, j);
+						east = isEastDoor(d, i , j);
+						
+						room = new DungeonRoom(items, monster, north, south, west, east, false, true, false);
+					} else {
+						north = isNorthDoor(d, i, j);
+						west = isWestDoor(d,i,j);
+						room = new DungeonRoom(items, monster, north, south, west, east, true, false, false);
+					}
+				}
+					
+				d[i][j] = room;	
+			}
+		}
 		
-		items = new ArrayList<Item>();
-		monster = mf.createMonster("Ogre");
-		items.add(If.createItem("heal")); 
-		room = new DungeonRoom(items, monster, true, false, false, true, false, false, false);
-		d[1][0] = room;
+		placePillars(d);
 		
-		monster = null;
-		items = new ArrayList<Item>();
-		items.add(If.createItem("heal"));
-		room = new DungeonRoom(items, monster, false, true, false, true, false, false, false);
-		d[2][0] = room;
-		
-		items = new ArrayList<Item>();
-		items.add(If.createItem("heal"));
-		items.add(If.createItem("encapsulation"));
-		room = new DungeonRoom(items, monster, true, false, false, true, false, false, false);
-		d[3][0] = room;
-		
-		items = new ArrayList<Item>();
-		monster = mf.createMonster("gremlin");
-		room = new DungeonRoom(items, monster, false, false, true, true, false, false, false);
-		d[0][1] = room;
-		
-		items = new ArrayList<Item>();
-		items.add(If.createItem("heal"));
-		monster = null;
-		room = new DungeonRoom(items, monster, false, true, true, true, false, false, false);
-		d[1][1] = room;
-		
-		items = new ArrayList<Item>();
-		items.add(If.createItem("heal"));
-		monster = null;
-		room = new DungeonRoom(items, monster, true,  false, true, true, false, false, false);
-		d[2][1] = room;
-		
-		items = new ArrayList<Item>();
-		room = new DungeonRoom(items, monster, false, false, true, true, false, false, false);
-		d[3][1] = room;
-		
-		items = new ArrayList<Item>();
-		room = new DungeonRoom(items, monster, false, true, true, true, false, false, false);
-		d[0][2] = room;
-		
-		items = new ArrayList<Item>();
-		items.add(If.createItem("trap"));
-		room = new DungeonRoom(items, monster, true, false, true, true, false, false, false);
-		d[1][2] = room;
-		
-		items = new ArrayList<Item>();
-		items.add(If.createItem("vision"));
-		room = new DungeonRoom(items, monster, false, false, true, true, false, false, false);
-		d[2][2] = room;
-		
-		//second to last
-		items = new ArrayList<Item>();
-		room = new DungeonRoom(items, monster, false, false, true, true, false, false, false);
-		d[3][2] = room;
-		
-		items = new ArrayList<Item>();
-		monster = mf.createMonster("skeleton");
-		items.add(If.createItem("inheritance"));
-		items.add(If.createItem("heal"));
-		
-		room = new DungeonRoom(items, monster, false, false, true, false, false, false, false);
-		d[0][3] = room;
-		
-		items = new ArrayList<Item>();
-		monster = mf.createMonster("skeleton");
-		items.add(If.createItem("polymorphism"));
-		room = new DungeonRoom(items, monster, false, false, true, false, false, false, false);
-		d[1][3] = room;
-		//above last
-		monster = null;
-		items = new ArrayList<Item>();
-		items.add(If.createItem("abstraction"));
-		room = new DungeonRoom(items, monster, false, true, true, false, false, false, false);
-		d[2][3] = room;
-		
-		monster = null;
-		items = new ArrayList<Item>();
-		room = new DungeonRoom(items, monster, true, false, true, false, true, false, false);
-		d[3][3] = room;
-		
+		if(isMazeTraversible(d) && isPillarsObtainable(d)) {
+			return d;
+		}
 
-		return d;
-
-}
+		return createDungeon();
+	}
 	
-  public static boolean isMazeTraversible(final DungeonRoom[][] theDungeon) {
+	private static boolean isNorthDoor(final DungeonRoom[][] theDungeon, final int theI, final int theJ) {
+		if(theI < 1) {
+			return false;
+		}
+		
+		return theDungeon[theI - 1][theJ].isSouth();
+	}
+	
+	private static boolean isWestDoor(final DungeonRoom[][] theDungeon, final int theI, final int theJ) {
+		if(theJ < 1) {
+			return false;
+		}
+		
+		return theDungeon[theI][theJ - 1].isEast();
+	}
+	
+	private static boolean isEastDoor(final DungeonRoom[][] theDungeon, final int theI, final int theJ) {
+		if(theJ > theDungeon[theI].length - 2) {
+			return false;
+		}
+		
+		int chance = Utility.randomNumberGen(0, 100);
+		if(chance < 70) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	private static boolean isSouthDoor(final DungeonRoom[][] theDungeon, final int theI, final int theJ) {
+		if(theI > theDungeon.length - 2) {
+			return false;
+		}
+		
+		int chance = Utility.randomNumberGen(0, 100);
+		if(chance < 70) {
+			return true;
+		}
+		
+		return false;
+	}
+	
+	public static void resetRoomsChecked(final DungeonRoom[][] theDungeon) {
+		for(int i = 0; i < theDungeon.length; i++) {
+			for(int j = 0; j < theDungeon[i].length; j++) {
+				theDungeon[i][j].setRoomChecked(false);
+			}
+		}
+	}
+	
+	public static void placePillars(final DungeonRoom[][] theDungeon) {
+		ItemFactory If = new ItemFactory();
+		int iMax = theDungeon.length - 2;
+		int jMax = theDungeon[1].length - 2;
+		
+		List<Item> list = new ArrayList<Item>();
+		list.add(If.createItem("abstraction"));
+		list.add(If.createItem("encapsulation"));
+		list.add(If.createItem("inheritance"));
+		list.add(If.createItem("polymorphism"));
+		
+		for(Item item: list) {
+			int i = Utility.randomNumberGen(1, iMax);
+			int j = Utility.randomNumberGen(1, jMax);
+			while(theDungeon[i][j].containsPillar()) {
+				i = Utility.randomNumberGen(1, iMax);
+				j = Utility.randomNumberGen(1, jMax);
+			}
+			
+			theDungeon[i][j].addItem(item);
+		}
+		
+	}
+	
+	public static boolean isMazeTraversible(final DungeonRoom[][] theDungeon) {
+		resetRoomsChecked(theDungeon);
 		return isMazeTraversible(theDungeon, 0, 0, "");
 	}
 	
@@ -152,6 +203,47 @@ public class Dungeon implements Serializable {
 			
 			if(!(theDir.equalsIgnoreCase("south")) && room.isSouth()) {
 				south = isMazeTraversible(theDungeon, theX + 1, theY, "north");
+			}
+			
+			
+		}
+		
+		return (north || south || east || west);
+	}
+	
+	public static boolean isPillarsObtainable(final DungeonRoom[][] theDungeon) {
+		resetRoomsChecked(theDungeon);
+		return isPillarsObtainable(theDungeon, 0, 0, "", 0);
+	}
+	
+	public static boolean isPillarsObtainable(final DungeonRoom[][] theDungeon, final int theX, final int theY, final String theDir, final int theCount) {
+		DungeonRoom room = theDungeon[theX][theY];
+		boolean north = false, south = false, east = false, west = false;
+		int count = theCount;
+		
+		if(theCount == 4) {
+			return true;
+		} else if (room.isChecked()) {
+			return false;
+		} else {
+			room.setRoomChecked(true);
+			if(room.containsPillar()) {
+				count++;
+			}
+			if(!(theDir.equalsIgnoreCase("north")) && room.isNorth()) {				
+				north = isPillarsObtainable(theDungeon, theX -  1, theY, "south", count);
+			}
+			
+			if(!(theDir.equalsIgnoreCase("east")) && room.isEast()) {
+				east = isPillarsObtainable(theDungeon, theX, theY + 1, "west", count);
+			}
+			
+			if(!(theDir.equalsIgnoreCase("west")) && room.isWest()) {
+				west = isPillarsObtainable(theDungeon, theX, theY - 1, "east", count);
+			}
+			
+			if(!(theDir.equalsIgnoreCase("south")) && room.isSouth()) {
+				south = isPillarsObtainable(theDungeon, theX + 1, theY, "north", count);
 			}
 			
 			
