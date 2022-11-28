879235f864d0dd29d2a027e5871b9701f33fbc7f
rileyjstev15 <rileyjstev15@gmail.com>
2022-11-28 10:13:47 -0800

    Java docs

--- DungeonAdventureProject/src/Model/Item.java
+++ DungeonAdventureProject/src/Model/Item.java
@@ -5,29 +5,43 @@ import java.io.Serializable;
 public class Item implements Serializable {
 	
 	/**
-	 * 
+	 * Serialization ID
 	 */
 	private static final long serialVersionUID = 1L;
 	
 	private final char  myType;
 	private final String myDescription;
 	private final boolean myItemUsable;
-	
+	/**
+	 * The constructor for the item
+	 * @param theType
+	 * @param theDescription
+	 * @param theItemUsable
+	 */
 	public Item(final char theType, final String theDescription, final boolean theItemUsable) {
 		myType = theType;
 		myDescription = theDescription;
 		myItemUsable = theItemUsable;
 	}
 	
-	
+	/**
+	 * The type of item
+	 * @return myType
+	 */
 	public char getType() {
 		return myType;
 	}
-	
+	/**
+	 * The item's description
+	 * @return myDescription
+	 */
 	public String getDescription() {
 		return myDescription;
 	}
-	
+	/**
+	 * Determines if the item is usable
+	 * @return myItemUsable
+	 */
 	public boolean isUsable() {
 		return myItemUsable;
 	}
--- DungeonAdventureProject/src/Model/Priestess.java
+++ DungeonAdventureProject/src/Model/Priestess.java
@@ -5,14 +5,20 @@ import Utility.Utility;
 public class Priestess extends Hero {
 	
 	/**
-	 * 
+	 * Serialization ID
 	 */
 	private static final long serialVersionUID = 1L;
-
+	/**
+	 * Constructor for the Priestess class
+	 * @param theCharacterName
+	 */
 	public Priestess(final String theCharacterName) {
 		super(75, 75, 25, 45, 0.7, 5, 0.3, "Priestess", theCharacterName, 0);
 	}
-	
+	/**
+	 * The chance for the priestess to heal
+	 * @param theCharacter
+	 */
 	@Override
 	public boolean special(final DungeonCharacter theCharacter) {
 		int newHealth = getHitPoints() + Utility.randomNumberGen(20,40);
